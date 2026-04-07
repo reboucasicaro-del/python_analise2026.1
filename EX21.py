39d0e85e12b3326fe6a6e38d767eba9602df581a
reboucasicaro-del <reboucas.icaro@gmail.com>
2026-04-06 21:16:12 -0300

    EX21.py

--- EX21.py
+++ EX21.py
@@ -7,6 +7,7 @@ elif s <= 8000:
     print(f"O salário é de R$ {s}, o desconto do IR é R$ {s*f1}, logo o valor liquido é de R$ {s-(s*f1)}")
 else:
     print(f"O salário é de R$ {s}, o desconto do IR é R$ {s*f2}, logo o valor liquido é de R$ {s-(s*f2)}")
+print("teste git")
 
 #if s <= 5000:
     #imposto = 0
