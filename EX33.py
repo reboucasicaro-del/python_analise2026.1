7dedde21f23c828b0e5e2c22d91930e7944f87d7
reboucasicaro-del <reboucas.icaro@gmail.com>
2026-04-13 19:03:28 -0300

    13.04.2026

---
+++ EX33.py
@@ -0,0 +1,5 @@
+# desenvolva um código python que leia, 3 valores, ao final mostre 
+# qual é o maior entre os três
+v1 = int(input("Valor I => "))
+v2 = int(input("Valor II => "))
+v3 = int(input("Valor III => "))
