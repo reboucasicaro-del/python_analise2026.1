9acc40a92477166825d046b0390c5de1753d4507
reboucasicaro-del <reboucas.icaro@gmail.com>
2026-04-17 20:34:37 -0300

    aula 17.04.2026

--- pd.py
+++ pd.py
@@ -1,5 +1,7 @@
 import pandas as pd
 df = pd.read_csv('ClassicDisco.csv')
-#print(df.to_string()) 
+#print(df.to_string()) >>> imprimi todos os dados da planilha
 #print(df.head()) >>>  #exibe as 5 primeiras linhas
-print(df.tail(10))
+#print(df.tail(10)) >>> imprimi as 5 ultimas linha do arquivo
+print(df[10:15]) #>>> entre [xx:xx] mostra o intervalo especificado 
+#print(df.shape) #indica o número de colunas e linhas do arquivo
